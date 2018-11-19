03e77e801162d835b7c66a6b28833567d6173cbc
David Kovacs <dkovacs@callibrity.com>
2018-11-19 13:32:26 -0500

    First Commit

---
+++ src/com/virtualpairprogrammers/learningkotlin/java/BoringJavaCode.java
@@ -0,0 +1,12 @@
+package com.virtualpairprogrammers.learningkotlin.java;
+
+public class BoringJavaCode {
+
+    public static void main(String args[]) {
+
+        String name = "Matt";
+
+        System.out.println(name);
+
+    }
+}
