c3bd0167f29324f3bd9351cb4a36d040828ce394
David Kovacs <dkovacs@callibrity.com>
2018-11-20 14:27:16 -0500

    End of Overriding 6.34

--- src/com/virtualpairprogrammers/learningkotlin/kotlin/Customer.kt
+++ src/com/virtualpairprogrammers/learningkotlin/kotlin/Customer.kt
@@ -1,42 +1,37 @@
 package com.virtualpairprogrammers.learningkotlin.kotlin
 
-//class Customer {
-//
-//    var name : String = "Matt"
-//    var address : String = "10 The High Street"
-//    var age : Int = 21
-//
-//    constructor(name: String, address:String, age: Int) {
-//        this.name = name
-//        this.address = address
-//        this.age = age
-//    }
-//}
 
 class Customer(val name : String, val address : String, var age : Int) {
 
-    constructor(name: String, age: Int) : this(name,"",age) {
-        println("secondary constructor")
-    }
+    constructor(name: String, age: Int) : this(name,"",age)
+
+}
+
+
+class AlternativeCustomer(val name: String, var age: Int) {
+    var address : String
 
     init {
-        println("init block")
+        address = ""
+    }
+
+    constructor(name: String, address: String, age: Int) : this(name, age) {
+        this.address = address
     }
 
 }
 
-//class Customer(val name : String, val address : String, var age : Int)
+class AnotherAlternativeCustomer(val name: String, var age: Int, val address: String = "")
 
-//class Customer(val name : String = "Matt", val address : String = "10 The High Street", var age : Int = 21)
 
 
 fun main(args: Array<String>) {
 
-    val customer = Customer("Matt", "10 The High Street", 21)
+    val customer = AnotherAlternativeCustomer("Matt", 21, "10 The High Street")
 
     customer.age = 22
 
-    val customer2 = Customer("David", 34)
+    val customer2 = AnotherAlternativeCustomer("David", 34)
 
     println("${customer.name} is ${customer.age} years old")
 
